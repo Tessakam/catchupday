cba814fb6e04f2c5dfbb5fba19a12e49a2282d2d
Tessakam <tessa.kam@hotmail.com>
2020-10-09 16:06:09 +0200

    experimented to link 2 php pages with each other

---
+++ addition.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<!-- Here is the HTML: -->
+<!-- back-end file -->
+<?php print_r($_GET)?>
+<?=$_GET['add_first'] + $_GET['add_second']?>
+<br><br>
+<a href="index.php">Reset</a>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -5,4 +5,29 @@ ini_set('display_errors', "1");
 ini_set('display_startup_errors', "1");
 error_reporting(E_ALL);
 
-echo "Hello Friday!";
+echo "Hello Friday! Time to calculate";
+
+//Made a small "calculator" to link 2 pages with each other.
+//index.php = front-end = fill in your numbers
+//addition.php = back-end = result of the addition + reset
+?>
+
+<!-- Here is the HTML: -->
+<!-- front-end file -->
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>Simple calculator</title>
+</head>
+<body>
+<h2>Addition</h2>
+<form action="addition.php"
+    method="get">
+    First number: <input type="number" name="add_first">
+    <br>
+    Second number: <input type="number" name="add_second">
+    <br>
+    <button type = "submit">Add</button>
+</form>
+
+
